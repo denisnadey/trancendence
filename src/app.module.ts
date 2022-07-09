57517b8a2646864d264e7da5cbaa3addd10c1818
denisdandy <denis.nadey@gmail.com>
2022-07-09 22:43:26 +0300

    feat: create docker compose for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'mdenys',
+      password: '12345678',
+      database: 'ping_pong',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
